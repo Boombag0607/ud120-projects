530a3946c45c9690b8e67bb98db920711669f5f2
Ananya Gautam <ananya@Ananyas-MacBook-Air.local>
2021-08-15 00:17:03 +0530

    svm miniproject initiate

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -22,7 +22,25 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 #########################################################
 ### your code goes here ###
+from sklearn.svm import SVC
 
+features_train = features_train[:int(len(features_train)/100)]
+labels_train = labels_train[:int(len(labels_train)/100)]
+
+clf = SVC(kernel = 'rbf')
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print('Training Time:', round(time()-t0, 3), 's')
+
+t0 = time()
+pred = clf.predict(features_test)
+print('Predicting Time:', round(time()-t0, 3), 's')
+
+from sklearn.metrics import accuracy_score
+
+acc = accuracy_score(pred, labels_test)
+print(acc)
 
 #########################################################
 
@@ -32,8 +50,10 @@ You'll be Provided similar code in the Quiz
 But the Code provided in Quiz has an Indexing issue
 The Code Below solves that issue, So use this one
 '''
-
-# features_train = features_train[:int(len(features_train)/100)]
-# labels_train = labels_train[:int(len(labels_train)/100)]
+# No. of Chris training emails :  7936
+# No. of Sara training emails :  7884
+# Training Time: 0.024 s
+# Predicting Time: 0.364 s
+# 0.8953356086461889
 
 #########################################################
